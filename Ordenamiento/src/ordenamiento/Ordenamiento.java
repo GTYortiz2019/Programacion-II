ada0a96610cd14e195c5ff5718348f89f70956a7
GTYortiz2019 <53540869+GTYortiz2019@users.noreply.github.com>
2019-07-31 23:14:23 -0600

    BURBUJA

--- Ordenamiento/src/ordenamiento/Ordenamiento.java
+++ Ordenamiento/src/ordenamiento/Ordenamiento.java
@@ -1,21 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ordenamiento;
 
-/**
- *
- * @author Ortiz Morales
- */
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Ordenamiento {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner var = new Scanner(System.in);//para ingresar datos con el teclado
+        Random nr=new Random();//generador de numeros random
+         int [] n= new int[25];//se inicializa el vector
+         int numero,aux;//variable en donde se almacenan los datos
+         
+         
+   for(int i=0;i<25;i++){ //inicio for para crear numeros random
+   numero=100+nr.nextInt(25); //indica de que rango seran nuestros numeros random
+   n[i]=numero;//se guarda el dato en una parte del vector
+    System.out.println("Datos del Arreglo: "+n[i]);//muestra los numeros que contiene el vector
+   }//fin for para crear numeros random
+ //inicio metodo burbuja
+ for(int i=0; i<(25-1);i++){//INICIO FOR VERIFICAR VECEES QUE TIENE QUE RECORRER EL VECTOR
+     for (int j=0; j<(25-1);j++){//INICIO FOR QUE ORDENA DATOS
+         if(n[j]>n[j+1]){//INICIO si el numero actual es mayor al numero siguiente realiza el cambio
+              aux=n[j];//variable auxiliar toma el valor del arreglo en primera posicion
+              n[j]=n[j+1];///primera posicion del arreglo toma el valor de la segunda posicion
+              n[j+1]= aux;//segunda posicion del arreglo toma el valor de auxiliar (primera posicion)
+        }//FIN IF si el numero actual es mayor al numero siguiente
+     }//FIN FOR QUE ORDENA DATOS
+ }//FIN FOR VERIFICAR VECEES QUE TIENE QUE RECORRER EL VECTOR
+ //fin metodo burbuja
+        
+        System.out.println("METODO BURBUJA ");
+ for(int i=0;i<25;i++){ //inicio for para crear numeros random
+    System.out.print("Datos del Arreglo: "+n[i]);//muestra los numeros que contiene el vector
+   }//fin for para crear numeros random
     }
+    
+    
+    
     
 }
